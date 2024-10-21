ad75bd7657f6569184716b979716f4b8ef3b4fba
MabsIPCA <a18829@alunos.ipca.pt>
2024-10-21 23:57:32 +0100

    inital k8s iteration

--- k8s/infra/traefik/install.sh
+++ k8s/infra/traefik/install.sh
@@ -1,4 +1,17 @@
 # Install Traefik Ingress Controller through Helm
-helm install traefik traefik/traefik --values values.yaml
-kubectl apply -f dashboard.yaml   
-kubectl port-forward $(kubectl get pods --selector "app.kubernetes.io/name=traefik" --output=name) 9000:9000
+helm install traefik traefik/traefik --values k8s/infra/traefik/values.yaml
+kubectl apply -f k8s/infra/traefik/dashboard.yaml
+
+kubectl apply -f k8s/envs
+kubectl apply -f k8s/db
+kubectl apply -f k8s/infra/keycloak
+kubectl apply -f k8s/infra/minio
+kubectl apply -f k8s/infra/rabbitmq
+kubectl apply -f k8s/mad
+kubectl apply -f k8s/services/docs
+kubectl apply -f k8s/services/lesson
+kubectl apply -f k8s/services/scoreboard
+kubectl apply -f k8s/services/webapp
+kubectl apply -f k8s/services/profile
+
+minikube service traefik -n kube-system
